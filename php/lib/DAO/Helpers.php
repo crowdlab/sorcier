6d56a542b02df48df9fe970f053d38390bc5b38f
Stanislav Vorobyev <vissi@vissi.su>
2013-08-05 11:19:08 +0200

    enrichAll operators
    Test Plan: -
    
    Reviewers: alex, max, mrkonstk
    
    Reviewed By: alex
    
    Differential Revision: https://review.crowd-lab.com/D1568

--- php/lib/DAO/Helpers.php
+++ php/lib/DAO/Helpers.php
@@ -6,12 +6,45 @@ use DAO;
  */
 trait Helpers {
 	/**
+	 * Выделить id из коллекции
+	 */
+	public static function getIds($r) {
+		$ids = [];
+		foreach ($r as $v) {
+			if (isset($v[static::IdKey]))
+				$ids []= $v[static::IdKey];
+		}
+		return $ids;
+	}
+
+	/**
 	 * Дополнить массив записей доп данными
 	 * TODO (vissi): медленно, лучше не использовать для большого количества записей
 	 */
 	public function enrichAll($r, $with = null) {
-		foreach($r as $k => $v)
+		$cond = [static::IdKey => ['$in' => static::getIds($r)]];
+		$op = $this->select_fn([static::IdKey], $cond);
+		foreach($with as $k => $it) {
+			$meth = "enrichAll_$it";
+			if (method_exists($this, $meth)) {
+				unset($with[$k]);
+				$op = $this->$meth($op);
+			}
+		}
+		$kv = [];
+		$ret = $this->fetch_all($op, function($item) use (&$kv) {
+			if (!isset($item[static::IdKey]))
+				return $item;
+			$id = $item[static::IdKey];
+			unset($item[static::IdKey]);
+			$kv[$id] = $item;
+			return $item;
+		});
+		foreach($r as $k => &$v) {
+			if (isset($r[static::IdKey]) && isset($kv[$r[static::IdKey]]))
+				$v = array_merge($v, $kv[$r[static::IdKey]]);
 			$r[$k] = $this->enrich($v, $with);
+		}
 		return $r;
 	}
 
