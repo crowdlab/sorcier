db2cb89b898acc05b5a0db887f2961c886a0a879
Stanislav Vorobyev <vissi@vissi.su>
2013-08-20 22:50:51 +0200

    func param for fetch operator calls

--- php/lib/DAO/MySQLOperator.php
+++ php/lib/DAO/MySQLOperator.php
@@ -250,25 +250,33 @@ class MySQLOperator {
 	/**
 	 * Если забыть сделать x
 	 */
-	public function fetch_all($schema = null) {
+	public function fetch_all($schema = null, $func = null) {
+		if (is_callable($schema)) {
+			$func = $schema;
+			$schema = null;
+		}
 		if (isset($this->dao) && $schema === null) {
 			$c = get_class($this->dao);
 			if (isset($c::$schema))
 				$schema = $c::$schema;
 		}
-		return $this->x()->fetch_all($schema);
+		return $this->x()->fetch_all($schema, $func);
 	}
 
 	/**
 	 * Если забыть сделать x
 	 */
-	public function fetch_assoc($schema = null) {
+	public function fetch_assoc($schema = null, $func = null) {
+		if (is_callable($schema)) {
+			$func = $schema;
+			$schema = null;
+		}
 		if (isset($this->dao) && $schema === null) {
 			$c = get_class($this->dao);
 			if (isset($c::$schema))
 				$schema = $c::$schema;
 		}
-		return $this->x()->fetch_assoc($schema);
+		return $this->x()->fetch_assoc($schema, $func);
 	}
 
 	/**
