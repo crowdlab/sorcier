8f57eb20dcd07fe5d66e4bc027ea923bece6284a
Stanislav Vorobyev <vissi@vissi.su>
2013-09-17 03:12:17 +0400

    pageable trait

---
+++ php/lib/DAO/Pageable.php
@@ -0,0 +1,45 @@
+<?php
+namespace DAO;
+use DAO;
+
+/**
+ * Pageable collections operations
+ */
+trait Pageable {
+	/**
+	 * Get pageable object
+	 *
+	 * @param $op    selection operator
+	 * @param $page  current page (1 by default)
+	 * @param $items_per_page
+	 * @param $field field to count by (id by default)
+	 * @return [
+	 *	'items' => [...],
+	 *	'pager' => ['no' => 5, 'current' => $page]
+	 * ]
+	 */
+	protected function getPageable($op, $page = 1, $items_per_page = 20,
+			$field = 'id') {
+		$cop = clone($op);
+		$my = $this instanceof MySQLDAO;
+		if ($my) {
+			$count = $cop
+				->select(\DAO\Sql\Expr::imbue("COUNT('$field')", 'c'))
+				->fetch_assoc()['c'];
+		} else {
+			$count = $op->x()->count();
+		}
+		$pager = [
+			'current' => (int) $page,
+			'no'      => int_divide($count, $items_per_page)
+						+ ($count % $items_per_page ? 1 : 0)
+		];
+		$op = $op->limit($items_per_page, ($page - 1) * $items_per_page);
+		$items = $my ? $op->fetch_all() : iterator_to_array($op->x());
+		return [
+			'items' => $items,
+			'pager' => $pager
+		];
+	}
+}
+?>
